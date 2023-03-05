6204efe83ff048d5f4cbfc80ab3c4e06a5124e86
mapawel <pawelma@gmail.com>
2023-03-05 12:54:46 +0100

    test for files-repo-service methods: createOne() and findAll() finished

--- src/animals/repositories/__test__/files-repo.service.spec.ts
+++ src/animals/repositories/__test__/files-repo.service.spec.ts
@@ -1,5 +1,7 @@
 import { FilesRepo } from '../files-repo.service';
 import { Setup } from '../../../../__tests__/global-test.setup';
+import { ConflictException } from '@nestjs/common';
+import { FilesRepoException } from '../exceptions/Files-repp.exception';
 
 describe('Files repository test suite:', () => {
   let setup: Setup;
@@ -30,6 +32,38 @@ describe('Files repository test suite:', () => {
 
       expect(setup.sortAnimalsById(await filesRepo.findAll())).toEqual(
         setup.sortAnimalsById([setup.animal1, setup.animal2]),
+      );
+    });
+
+    it('should throw ConflictException on try to save an animal with not unique insurance ID', async () => {
+      //given
+      await filesRepo.createOne(setup.animal1);
+
+      //when+then
+      await expect(
+        async () =>
+          await filesRepo.createOne({
+            ...setup.animal2,
+            insuranceId: setup.animal1.insuranceId,
+          }),
+      ).rejects.toThrow(ConflictException);
+    });
+
+    it('should throw FilesRepoException on try to save an animal to non existing folder', async () => {
+      //given
+      const anotherFilesRepo = new FilesRepo(['non', 'existing']);
+      // when+then
+      await expect(
+        async () => await anotherFilesRepo.createOne(setup.animal1),
+      ).rejects.toThrow(FilesRepoException);
+    });
+
+    it('should throw FilesRepoException on try to findAll from non existing folder', async () => {
+      //given
+      const anotherFilesRepo = new FilesRepo(['non', 'existing']);
+      // when+then
+      await expect(async () => anotherFilesRepo.findAll()).rejects.toThrow(
+        FilesRepoException,
       );
     });
   });
